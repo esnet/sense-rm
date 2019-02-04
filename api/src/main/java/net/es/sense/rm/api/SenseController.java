adc0136c1a571289a889b5c6944ffae788d16219
John MacAuley <john@blackacorn.ca>
2019-02-04 13:22:40 -0500

    Fixed parameter type in ExceptionHandler.

--- api/src/main/java/net/es/sense/rm/api/SenseController.java
+++ api/src/main/java/net/es/sense/rm/api/SenseController.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package net.es.sense.rm.api;
 
 import java.net.MalformedURLException;
@@ -12,15 +6,17 @@ import java.util.NoSuchElementException;
 import lombok.extern.slf4j.Slf4j;
 import net.es.sense.rm.api.common.Error;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 /**
- * Exception	HTTP Status Code
+ * Exception
+ * HTTP Status Code
  * ConversionNotSupportedException	500 (Internal Server Error)
  * HttpMediaTypeNotAcceptableException	406 (Not Acceptable)
- * HttpMediaTypeNotSupportedException	415 (Unsupported Media Type)
+ * HttpMediaTypeNotSupportedException	415 (Unsupported MediaType)
  * HttpMessageNotReadableException	400 (Bad Request)
  * HttpMessageNotWritableException	500 (Internal Server Error)
  * HttpRequestMethodNotSupportedException	405 (Method Not Allowed)
@@ -38,18 +34,27 @@ public abstract class SenseController {
    * @param ex
    * @return
    */
-  @ExceptionHandler({ MalformedURLException.class, URISyntaxException.class, Exception.class })
+  @ExceptionHandler({MalformedURLException.class, URISyntaxException.class, Exception.class})
   @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
   @ResponseBody
-  public Error handleMalformedUrlException(MalformedURLException ex) {
+  public ResponseEntity<?> handleMalformedUrlException(Exception ex) {
     log.error("handleMalformedUrlException: ", ex);
-    return new Error(HttpStatus.INTERNAL_SERVER_ERROR.toString(), ex.getLocalizedMessage(), null);
+    Error error = Error.builder()
+            .error(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase())
+            .error_description(ex.getMessage())
+            .build();
+    return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
   }
 
   @ExceptionHandler(NoSuchElementException.class)
   @ResponseStatus(value = HttpStatus.NOT_FOUND)
-  public Error handleResourceNotFoundException(NoSuchElementException ex) {
+  @ResponseBody
+  public ResponseEntity<?> handleResourceNotFoundException(NoSuchElementException ex) {
     log.error("handleResourceNotFoundException: ", ex);
-    return new Error(HttpStatus.NOT_FOUND.toString(), ex.getLocalizedMessage(), null);
+    Error error = Error.builder()
+            .error(HttpStatus.NOT_FOUND.getReasonPhrase())
+            .error_description(ex.getMessage())
+            .build();
+    return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
   }
 }

c9dfd0790086f1186aeb33e35c142632e000001a
John MacAuley <john@blackacorn.ca>
2022-01-27 12:47:49 -0500

    Added new nsi-aggr.es.net certificate.

--- certificates/build_keystore.sh
+++ certificates/build_keystore.sh
@@ -5,62 +5,75 @@
 #
 
 # Verify the correct number of parameters.
-if [[ "$#" -ne 5 ]]; then
+if [[ "$#" -ne 6 ]]; then
     echo "ERROR: Illegal number of parameters"
-    echo "Usage: $0 <keystorefile> <passwd> <keyfile> <certfile> <ca-file>"
+    echo "Usage: $0 <keystoretype> <keystorefile> <passwd> <keyfile> <certfile> <ca-file>"
+    exit 2
+fi
+
+# Verify this is a supported keystore type.
+if [[ "$1" != "JKS" && "$1" != "PKCS12" ]]; then
+    echo "Error: <storetype> must be one of JKS or PKCS12"
     exit 2
 fi
 
 # Remove the keystore file if it already exists.
-if [[ -f "$1" ]]; then
-    echo "Deleting $1"
-    rm -f $1
+if [[ -f "$2" ]]; then
+    echo "Deleting $2"
+    rm -f $2
 fi
 
 # Check if a vaild key file was specified.
-if [[ ! -f "$3" ]]; then
-    echo "ERROR: $3 does not exist"
-    exit 2
-fi
-
-# Check if a vaild certificate file was specified.
 if [[ ! -f "$4" ]]; then
     echo "ERROR: $4 does not exist"
     exit 2
 fi
 
-# Check if a vaild CA file was specified.
+# Check if a vaild certificate file was specified.
 if [[ ! -f "$5" ]]; then
     echo "ERROR: $5 does not exist"
+    exit 2
+fi
+
+# Check if a vaild CA file was specified.
+if [[ ! -f "$6" ]]; then
+    echo "ERROR: $6 does not exist"
     exit 2
 fi
 
 # We need to generate a temporary file name for this process.
 tfile=$(mktemp /tmp/foo.XXXXXXXXX)
 
-# Create a temporary pkcs12 keystore. 
+# Create a temporary pkcs12 keystore.
 openssl pkcs12 -export \
     -out $tfile \
-    -inkey $3 \
-    -in $4 \
-    -certfile $5 \
-    -passout pass:"$2"
+    -inkey $4 \
+    -in $5 \
+    -certfile $6 \
+    -passout pass:"$3"
 
-# Convert the temporary pkcs12 keystore to JKS.
-$JAVA_HOME/bin/keytool -v -importkeystore \
-    -noprompt \
-    -srcstorepass "$2" \
-    -deststorepass "$2"  \
-    -srckeystore $tfile \
-    -srcstoretype PKCS12 \
-    -deststoretype JKS \
-    -destkeystore $1
-
-# Cleanup.
-rm -f $tfile
+if [[ "$1" == "JKS" ]]; then
+  # Convert the temporary pkcs12 keystore to JKS.
+  $JAVA_HOME/bin/keytool -v -importkeystore \
+      -noprompt \
+      -srcstorepass "$3" \
+      -deststorepass "$3"  \
+      -srckeystore $tfile \
+      -srcstoretype PKCS12 \
+      -deststoretype JKS \
+      -destkeystore $2
+  rm -f $tfile
+else
+    mv -f $tfile $2
+fi
 
 #
 # We are done
 #
-echo "COMPLETED: created keystore $1"
+if [[ -f "$2" ]]; then
+    echo "COMPLETED: created keystore $2"
+else
+    echo "Error: failed to create keystore $2"
+    exit 2
+fi
 
--- certificates/build_truststore.sh
+++ certificates/build_truststore.sh
@@ -1,51 +1,66 @@
 #!/bin/bash
 
 #
-# A simple script to build a Java truststore from PEM files in the current directory.
+# A simple script to build a truststore from PEM files in the current directory.
 #
 
-if [[ "$#" -ne 3 ]]; then
+# Verify the correct number of parameters.
+if [[ "$#" -ne 4 ]]; then
     echo "ERROR: Illegal number of parameters"
-    echo "Usage: $0 <truststorefile> <password> <directory>"
+    echo "Usage: $0 <truststoretype> <truststorefile> <password> <directory>"
     exit 2
 fi
 
-if [[ -f "$1" ]]; then
-    echo "Deleting $1"
-    rm -f $1
+# Verify this is a supported truststore type.
+if [[ "$1" != "JKS" && "$1" != "PKCS12" ]]; then
+    echo "Error: <storetype> must be one of JKS or PKCS12"
+    exit 2
 fi
 
-target="$(dirname "$1")"
+# Remove the truststore file if it already exists.
+if [[ -f "$2" ]]; then
+    echo "Deleting $2"
+    rm -f $2
+fi
+
+# If the target director does not exist then create it.
+target="$(dirname "$2")"
 if [[ ! -d "$target" ]]; then
     echo "Creating directory $target"
     mkdir -p "$target"
 fi
 
-if [[ ! -d "$3" ]]; then
-    echo "ERROR: $3 not a directory"
+if [[ ! -d "$4" ]]; then
+    echo "ERROR: $4 not a directory"
     exit 2
 fi
 
-for file in `ls $3/*.pem`
+for file in `ls $4/*.pem`
 do
   filename="${file##*/}"
   alias="${filename%.*}"
 
   echo "Adding $file as alias $alias"
 
-  openssl x509 -in $file -text | grep -i "Subject: " | cut -d':' -f2- 
+  openssl x509 -in $file -text | grep -i "Subject: " | cut -d':' -f2-
 
   $JAVA_HOME/bin/keytool -importcert \
-        -keystore $1 \
-        -storepass "$2" \
+        -keystore $2 \
+        -storepass "$3" \
         -alias $alias \
         -trustcacerts \
         -file $file \
         -noprompt \
-        -deststoretype JKS
-        #-deststoretype PKCS12
+        -deststoretype $1
+        # -deststoretype PKCS12
+        # -deststoretype JKS
 
 done
 
-echo "COMPLETED: created truststore $1"
+if [[ -f "$2" ]]; then
+    echo "COMPLETED: created truststore $2"
+else
+    echo "Error: no PEM files present in directory $4"
+    exit 2
+fi
 
